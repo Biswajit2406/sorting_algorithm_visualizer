00fc43d334c2c0402af59772a5d693a3bdee0fd9
Biswajit Ghosh <ghoshbiswajit2406@gmail.com>
2022-09-11 22:24:12 +0530

    Randomize array

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function renderBars(array) {
 }
 
 randomize_array.addEventListener("click", function () {
-    createRandomArray();
+    unsorted_array=createRandomArray();
     bars_container.innerHTML = ""
     renderBars(unsorted_array)
 })
